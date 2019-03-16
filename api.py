738bf0ef5a976232fbf18e9bd559057d64cb8b6c
robmarkcole <robmarkcole@gmail.com>
2019-03-16 20:22:54 +0000

    Black format

--- api.py
+++ api.py
@@ -4,60 +4,46 @@ from datetime import datetime, timedelta
 from requests import get
 import json
 
-CURRENCY_API_KEY = '834dbbebf0270311fc5d'
+CURRENCY_API_KEY = "834dbbebf0270311fc5d"
 
 raw_pricing = {
     "prices": [
-        {
-            "product_id": 1,
-            "price": 599,
-            "vat_band": "standard"
-        },
-        {
-            "product_id": 2,
-            "price": 250,
-            "vat_band": "zero"
-        },
-        {
-            "product_id": 3,
-            "price": 250,
-            "vat_band": "zero"
-        },
-        {
-            "product_id": 4,
-            "price": 1000,
-            "vat_band": "zero"
-        },
-        {
-            "product_id": 5,
-            "price": 1250,
-            "vat_band": "standard"
-        }
+        {"product_id": 1, "price": 599, "vat_band": "standard"},
+        {"product_id": 2, "price": 250, "vat_band": "zero"},
+        {"product_id": 3, "price": 250, "vat_band": "zero"},
+        {"product_id": 4, "price": 1000, "vat_band": "zero"},
+        {"product_id": 5, "price": 1250, "vat_band": "standard"},
     ],
-    "vat_bands": {
-        "standard": 0.2,
-        "zero": 0
-    }
+    "vat_bands": {"standard": 0.2, "zero": 0},
 }
 
-conversion_rates = {'fake_currency': {'rate': 1.3,
-                                      'updated': datetime.now()}}  # init and show contents
+conversion_rates = {
+    "fake_currency": {"rate": 1.3, "updated": datetime.now()}
+}  # init and show contents
 
 app = Flask(__name__)
 api = Api(app)
 
 
 def format_pricing(raw_pricing: dict):
-    return {product["product_id"]: {"price": product["price"],
-                                    "VAT": round(raw_pricing["vat_bands"][product["vat_band"]]*product["price"], 2)
-                                    } for product in raw_pricing["prices"]}
+    return {
+        product["product_id"]: {
+            "price": product["price"],
+            "VAT": round(
+                raw_pricing["vat_bands"][product["vat_band"]] * product["price"], 2
+            ),
+        }
+        for product in raw_pricing["prices"]
+    }
 
 
 def format_order(order: dict):
     return {item["product_id"]: item["quantity"] for item in order["order"]["items"]}
 
 
-def calc_order_details(order_quantities: dict, pricing: dict, currency : str, conversion_rate: float = 1.0):
+def calc_order_details(
+    order_quantities: dict, pricing: dict, currency: str, conversion_rate: float = 1.0
+):
     summary = {}
     running_total_price = 0
     running_total_vat = 0
@@ -65,13 +51,13 @@ def calc_order_details(order_quantities: dict, pricing: dict, currency : str, co
     for product_id in order_quantities:
         summary[product_id] = {}
         quantity = order_quantities[product_id]
-        summary[product_id]['quantity'] = quantity
+        summary[product_id]["quantity"] = quantity
 
-        total_price = quantity * pricing[product_id]['price'] * conversion_rate
+        total_price = quantity * pricing[product_id]["price"] * conversion_rate
         summary[product_id]["total_price"] = round(total_price, 2)
         running_total_price += total_price
 
-        total_vat = quantity * pricing[product_id]['VAT'] * conversion_rate
+        total_vat = quantity * pricing[product_id]["VAT"] * conversion_rate
         summary[product_id]["total_VAT"] = round(total_vat, 2)
         running_total_vat += total_vat
 
@@ -87,21 +73,19 @@ def get_conversion_rate(currency: str, conversion_rates: dict = conversion_rates
     
     currency : 3 char str
     """
-    if currency == 'GBP':
+    if currency == "GBP":
         return 1.0
 
     if currency in conversion_rates.keys():
         if conversion_rates[currency]["updated"] > datetime.now() - timedelta(hours=1):
             # is up to date, return the rate
-            return conversion_rates[currency]['rate']
+            return conversion_rates[currency]["rate"]
 
-    query = f'GBP_{currency}'
-    url = 'https://free.currencyconverterapi.com/api/v6/convert?'
-    rate = get(f'{url}apiKey={CURRENCY_API_KEY}&q={query}&compact=ultra').json()[
-        query]
+    query = f"GBP_{currency}"
+    url = "https://free.currencyconverterapi.com/api/v6/convert?"
+    rate = get(f"{url}apiKey={CURRENCY_API_KEY}&q={query}&compact=ultra").json()[query]
     conversion_rates[currency] = {"rate": rate, "updated": datetime.now()}
     return rate
-
 
 
 class PricingAPI(Resource):
@@ -116,11 +100,12 @@ class PricingAPI(Resource):
         order_quantities = format_order(order)
         pricing = format_pricing(raw_pricing)
         order_details = calc_order_details(
-            order_quantities, pricing, currency, conversion_rate)
+            order_quantities, pricing, currency, conversion_rate
+        )
         return order_details
 
 
-api.add_resource(PricingAPI, '/<string:order>')
+api.add_resource(PricingAPI, "/<string:order>")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
